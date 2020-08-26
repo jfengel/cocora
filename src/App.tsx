faf363b403effadd30150f768ed0bb8798ef674c
Joshua Engel <engel@purgo.net>
2020-08-26 16:44:30 -0400

    Add nearby places lookup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Geosuggest from 'react-geosuggest';
 import 'react-geosuggest/module/geosuggest.css'
 import './App.css';
 import Map from './components/Map'
 
 const GEOLOCATION_UPDATE_FREQUENCY_MSEC = 1000;
+const SEARCH_RADIUS_METERS = 500;
 
 function App() {
     const [location, setLocation] = useState();
-    const [position, setPosition] = useState<google.maps.LatLng>();
+    const [currentPosition, setCurrentPosition] = useState<google.maps.LatLng>();
+    const [nearbyPlaces, setNearbyPlaces] = useState<google.maps.places.PlaceResult[]>([]);
+    const positionRef = useRef(currentPosition);
+
+    const findNearbyPlaces = (current : google.maps.LatLng) => {
+        const location = current || positionRef.current;
+        if (location) {
+            console.info("Looking for nearby things at", location);
+            const placesService = new google.maps.places.PlacesService(document.createElement('div'));
+            placesService.nearbySearch({
+                location,
+                radius: SEARCH_RADIUS_METERS,
+                // rankBy: google.maps.places.RankBy.DISTANCE
+            }, callback);
+        }
+    }
 
     useEffect(() => {
         if (navigator.geolocation) {
@@ -19,34 +35,60 @@ function App() {
                     if (!isNaN(longitude) && !isNaN(latitude)) {
                         const now = new Date().valueOf()
                         if (now - lastUpdate > GEOLOCATION_UPDATE_FREQUENCY_MSEC) {
-                            setPosition(new google.maps.LatLng(latitude, longitude));
+                            // TODO also set a minimum update distance
+                            const here = new google.maps.LatLng(latitude, longitude);
+                            setCurrentPosition(here);
                             lastUpdate = now;
+                            if(!positionRef.current) {
+                                findNearbyPlaces(here);
+                            }
+                            positionRef.current = here;
                         }
                     }
                 },
                 function error(msg) {
                     console.error('Geolocation error', msg.message);
                 },
-                {enableHighAccuracy: true});
+                {enableHighAccuracy: false});
         } else {
             // alert("Geolocation API is not supported in your browser.");
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    const currentPosition : [number, number] | undefined = position && [position.lat(), position.lng()]
+    const callback: (results: google.maps.places.PlaceResult[],
+                     status: google.maps.places.PlacesServiceStatus) => void =
+        (result, status) => {
+            if(status === google.maps.places.PlacesServiceStatus.OK) {
+                setNearbyPlaces(result)
+            } else {
+                console.error('Could not find nearby places', status)
+            }
+        }
+
+    useEffect(() => {
+        setInterval(findNearbyPlaces, 30000)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return <div className="App">
         <Geosuggest
             onSuggestSelect={(e) => setLocation(e)}
-            location={position}
-            radius={position && 100}
+            location={currentPosition}
+            radius={currentPosition && 100}
         />
         <p>
             {location && location.gmaps.icon && <img src={location.gmaps.icon} alt={""}/>}
             {location && location.gmaps.name}
         </p>
+        <ul>
+            {nearbyPlaces
+                .filter((_, i) => i < 4)
+                .map((place, i) => <li key={i}>{place.name}</li>)}
+        </ul>
         <div className="mapContainer">
             <Map
-                currentPosition={currentPosition}
+                currentPosition={currentPosition && [currentPosition.lat(), currentPosition.lng()]}
                 locations={[]}
             />
         </div>
