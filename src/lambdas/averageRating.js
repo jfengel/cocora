52bc0b43ff2b4b6cdcc295a91a0ef6a9b1ada433
Joshua Engel <engel@purgo.net>
2020-09-05 14:37:09 -0400

    Try connection pool

--- src/lambdas/averageRating.js
+++ src/lambdas/averageRating.js
@@ -1,31 +1,41 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
     host: process.env.COCORA_DATABASE_HOST,
     user: 'admin',
     password: process.env.COCORA_DATABASE_PASSWORD
 });
 
-connection.connect(null, (result) => console.info('connection', result));
+// connection.connect(null, (result) => console.info('connection', result));
 
 exports.handler = (event, context, callback) => {
     const placeid = event.path.split('/').reverse()[0]
-    connection.query("SELECT AVG(rating) AS rating FROM cocora.ratings WHERE placeid = ? ",
-        [placeid],
-        (err, rows) => {
-            if (err) {
-                callback(null, {
-                    statusCode: 500,
-                    body: JSON.stringify(err, null, 4)
-                })
-            } else {
-                const {rating} = rows[0];
-                callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify({rating})
-                })
-            }
-        });
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error', err);
+            callback(null, {
+                statusCode: 500,
+                body: JSON.stringify(JSON.stringify(err))
+            })
+            return;
+        }
+        connection.query("SELECT AVG(rating) AS rating FROM cocora.ratings WHERE placeid = ? ",
+            [placeid],
+            (err, rows) => {
+                if (err) {
+                    callback(null, {
+                        statusCode: 500,
+                        body: JSON.stringify(err, null, 4)
+                    })
+                } else {
+                    const {rating} = rows[0];
+                    callback(null, {
+                        statusCode: 200,
+                        body: JSON.stringify({rating})
+                    })
+                }
+            })
+    })
 
 
 };
