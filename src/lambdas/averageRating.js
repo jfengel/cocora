1e7c90a67b83491f5dfe80353288c87e36742562
Joshua Engel <engel@purgo.net>
2020-09-06 12:17:34 -0400

    Trying a promise

--- src/lambdas/averageRating.js
+++ src/lambdas/averageRating.js
@@ -8,43 +8,52 @@ var pool = mysql.createPool({
 
 // connection.connect(null, (result) => console.info('connection', result));
 
-exports.handler = (event, context, callback) => {
-    const placeid = event.path.split('/').reverse()[0]
-    pool.getConnection((err, connection) => {
-        if (err) {
-            console.error('Error', err);
-            callback(null, {
-                statusCode: 500,
-                body: JSON.stringify(JSON.stringify(err))
-            })
-            return;
-        }
-        console.info('connecting');
-        connection.connect();
-        console.info('connected');
+exports.handler = async (event, context, callback) => {
+    const p = new Promise((resolve, _) => {
+        const placeid = event.path.split('/').reverse()[0]
+        pool.getConnection((err, connection) => {
+            if (err) {
+                console.error('Error', err);
+                callback(null, {
+                    statusCode: 500,
+                    body: JSON.stringify(JSON.stringify(err))
+                })
+                resolve();
+                return;
+            }
+            console.info('connecting');
+            connection.connect();
+            console.info('connected');
 
-        // noinspection SqlResolve
-        connection.query("SELECT AVG(rating) AS rating FROM cocora.ratings WHERE placeid = ? ",
-            [placeid],
-            (err, rows) => {
-                console.info('query',  JSON.stringify(rows));
-                console.info('error', JSON.stringify(err));
-                if (err) {
-                    callback(null, {
-                        statusCode: 500,
-                        body: JSON.stringify(err, null, 4)
-                    })
-                } else {
-                    connection.release();
-                    const {rating} = rows[0];
-                    callback(null, {
-                        statusCode: 200,
-                        body: JSON.stringify({rating})
-                    })
-                    console.info('callback called back')
-                }
-            })
-    })
+            // noinspection SqlResolve
+            connection.query("SELECT AVG(rating) AS rating FROM cocora.ratings WHERE placeid = ? ",
+                [placeid],
+                (err, rows) => {
+                    console.info('query', JSON.stringify(rows));
+                    console.info('error', JSON.stringify(err));
+                    if (err) {
+                        callback(null, {
+                            statusCode: 500,
+                            body: JSON.stringify(err, null, 4)
+                        })
+                    } else {
+                        connection.release();
+                        const {rating} = rows[0];
+                        callback(null, {
+                            statusCode: 200,
+                            body: JSON.stringify({rating})
+                        })
+                        console.info('callback called back')
+                    }
+                    console.info('Resolving')
+                    resolve();
+                    console.info('Resolved');
+
+                })
+        })
+    });
+    await p;
+
     console.info('REturned');
 
 
